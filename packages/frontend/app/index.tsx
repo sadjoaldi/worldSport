2de087e621341aab56d2241f76d3086187d54483
sadjoaldi <alsannealdi@hotmail.com>
2025-09-10 03:10:20 +0200

    monorepo react native expo, with express

--- packages/frontend/app/index.tsx
+++ packages/frontend/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Home() {
   return (
     <View>
-      <Text>Bienvenue sur la page d'accueil de worldsport international</Text>
+      <Text>Bienvenue sur la page d'acde worldsport international</Text>
     </View>
   );
 }
