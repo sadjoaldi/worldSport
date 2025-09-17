fa7b2a277d6005b248b148942b43306e81122152
sadjoaldi <alsannealdi@hotmail.com>
2025-09-17 04:40:54 +0200

    recup and display leagues and country name

--- packages/frontend/app/(tabs)/(sport)/all-competitions.tsx
+++ packages/frontend/app/(tabs)/(sport)/all-competitions.tsx
@@ -1,23 +1,56 @@
-import { StyleSheet, Text, View } from "react-native";
+import { trpc } from "@/trpc";
+import { useQuery } from "@tanstack/react-query";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function AllCompetitions() {
+  const countryFootballLeagues = useQuery(
+    trpc.football.getCountryLeagues.queryOptions()
+  );
+
+  const { data: countryLeagues, isLoading } = countryFootballLeagues;
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Loading...</Text>
+      </View>
+    );
+  }
+
+  if (!countryLeagues) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>No Competitions Found</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>All Competitions</Text>
-      <Text style={styles.text}>All Competitions</Text>
-      <Text style={styles.text}>All Competitions</Text>
-      <Text style={styles.text}>All Competitions</Text>
-      <Text style={styles.text}>All Competitions</Text>
-      <Text style={styles.text}>All Competitions</Text>
-      <Text style={styles.text}>All Competitions</Text>
-      <Text style={styles.text}>All Competitions</Text>
-      <Text style={styles.text}>All Competitions</Text>
-      <Text style={styles.text}>All Competitions</Text>
-      <Text style={styles.text}>All Competitions</Text>
-      <Text style={styles.text}>All Competitions</Text>
-      <Text style={styles.text}>All Competitions</Text>
-      <Text style={styles.text}>All Competitions</Text>
-      <Text style={styles.text}>All Competitions</Text>
+      {isLoading ? (
+        <Text style={styles.text}>Loading...</Text>
+      ) : (
+        <Text style={styles.text}>Loaded Competitions</Text>
+      )}
+
+      {countryLeagues.map(({ league, country }) => (
+        <View
+          key={league.id}
+          style={{
+            marginBottom: 20,
+            alignItems: "center",
+            flexDirection: "row",
+            gap: 10,
+          }}
+        >
+          <Image
+            source={{ uri: league.logo }}
+            style={{ width: 32, height: 32 }}
+          />
+          <Text style={styles.text}>{league.name}</Text>
+          <Text style={styles.text}>{country.name}</Text>
+        </View>
+      ))}
     </View>
   );
 }
